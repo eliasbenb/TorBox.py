a1972d034066fb48809d94a1174bea0fa73deff4
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-11-22 22:40:36 +0000

    feat: Implemented the Usenet API wrappers

--- torbox/client.py
+++ torbox/client.py
@@ -1,5 +1,5 @@
 from .services.base import BaseService
-from .services.torrents import TorrentsService
+from .services import TorrentsService, UsenetService
 
 
 class TorBox(BaseService):
@@ -8,3 +8,4 @@ class TorBox(BaseService):
     def __init__(self, api_key: str, base_url: str = "https://api.torbox.app/v1"):
         super().__init__(api_key, base_url)
         self.torrents = TorrentsService(self._api_key, self._base_url, self._session)
+        self.usenet = UsenetService(self._api_key, self._base_url, self._session)
--- torbox/services/__init__.py
+++ torbox/services/__init__.py
@@ -1,2 +1,3 @@
 from .base import BaseService as BaseService
 from .torrents import TorrentsService as TorrentsService
+from .usenet import UsenetService as UsenetService

9f64d6c1c78b75f4a08483640863fba5989ea4d0
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-11-22 22:41:18 +0000

    feat: Implemented the WebDL API wrappers

--- torbox/client.py
+++ torbox/client.py
@@ -1,5 +1,5 @@
 from .services.base import BaseService
-from .services import TorrentsService, UsenetService
+from .services import TorrentsService, UsenetService, WebDLService
 
 
 class TorBox(BaseService):
@@ -7,5 +7,7 @@ class TorBox(BaseService):
 
     def __init__(self, api_key: str, base_url: str = "https://api.torbox.app/v1"):
         super().__init__(api_key, base_url)
+
         self.torrents = TorrentsService(self._api_key, self._base_url, self._session)
         self.usenet = UsenetService(self._api_key, self._base_url, self._session)
+        self.webdl = WebDLService(self._api_key, self._base_url, self._session)
--- torbox/services/__init__.py
+++ torbox/services/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseService as BaseService
 from .torrents import TorrentsService as TorrentsService
 from .usenet import UsenetService as UsenetService
+from .webdl import WebDLService as WebDLService
